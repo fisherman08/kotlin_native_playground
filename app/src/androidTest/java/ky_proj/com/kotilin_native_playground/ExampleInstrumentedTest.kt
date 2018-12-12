4e4712a04a3ad0e2f5a492151c1067df545fe876
ky <orenoikizama@hotmail.com>
2018-12-13 08:39:39 +0900

    create project

---
+++ app/src/androidTest/java/ky_proj/com/kotilin_native_playground/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package ky_proj.com.kotilin_native_playground
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("ky_proj.com.kotilin_native_playground", appContext.packageName)
+    }
+}
